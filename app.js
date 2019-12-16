19ac347ca5cdd5fb85dd012a56a01a551383abcf
Marfa (Mory Bamba) <mory.bamba1@gmail.com>
2019-12-16 17:42:33 +0100

    Refactoring, add 'tchip command' and choice dice sides

--- app.js
+++ app.js
@@ -1,63 +1,47 @@
 const tmi = require('tmi.js');
-const dotenv = require('dotenv');
-dotenv.config();
-
-// console.log(process.env.BOT_USERNAME);
-// console.log(process.env.OAUTH_TOKEN);
-// console.log(process.env.CHANNEL_NAME);
-
-// Define configuration options
-const opts = {
-  options: { debug: true },
-  connection: {
-		secure: true,
-		reconnect: true
-	},
-  identity: {
-    username: process.env.BOT_USERNAME,
-    password: process.env.OAUTH_TOKEN
-  },
-  channels: [
-    process.env.CHANNEL_NAME
-  ]
-};
+const options = require("./config.js");
+const commands = require("./commands.js");
+const CMD_PREFIX = "!";
 
 // Create a client with our options
-const client = new tmi.client(opts);
-
-// Register our event handlers (defined below)
-client.on('message', onMessageHandler);
-client.on('connected', onConnectedHandler);
+const client = new tmi.client(options);
 
 // Connect to Twitch:
 client.connect();
 
-// Called every time a message comes in
-function onMessageHandler (target, context, msg, self) {
-  if (self) { return; } // Ignore messages from the bot
-
-  // Remove whitespace from chat message
-  const commandName = msg.trim();
-
-  // If the command is known, let's execute it
-  if (commandName === '!d20') {
-    const num = rollDice(commandName);
-    client.say(target, `You rolled a ${num}.`);
-    console.log(`* Executed ${commandName} command`);
-  } else if (commandName === '!hello') {
-    client.say(target, `@${context.username}, heya!`);
-  } else {
-    console.log(`* Unknown command ${commandName}`);
-  }
-}
-
-// Function called when the "dice" command is issued
-function rollDice () {
-  const sides = 20;
-  return Math.floor(Math.random() * sides) + 1;
-}
+// Register our event handlers (defined below)
+// client.on('message', onMessageHandler);
+client.on('connected', onConnectedHandler);
 
 // Called every time the bot connects to Twitch chat
 function onConnectedHandler (addr, port) {
   console.log(`* Connected to ${addr}:${port}`);
+}
+
+client.on("chat", function (channel, userstate, message, self) {
+  if (self) return;
+  const msg = message.trim();
+  console.log(userstate, msg);
+  handleCommmand(channel, userstate, msg);
+});
+
+function handleCommmand(channel, userstate, message) {
+  if (message.charAt(0) == CMD_PREFIX) {
+    processCommand(channel, userstate, message)
+  }
+}
+
+function processCommand(channel, userstate, message) {
+  console.log("processing commands...");
+  message = message.replace(CMD_PREFIX, '').toLowerCase();
+  command = message.split(' ');
+  for (let i = 0; i < commands.length; i++) {
+    if (command[0] == commands[i].name) {
+      if (commands[i].type == "function") {
+        commands[i].func(channel, userstate, message, client, command[1]);
+      } else if (commands[i].type == "message") {
+        client.say(channel, commands[i].message);
+      }
+    }
+  }
 }
---
+++ commands.js
@@ -0,0 +1,28 @@
+const commands = [
+    {
+        name: "hello",
+        type: "function",
+        func: function (channel, userstate, message, client, sides = 20) {
+            client.say(channel, `Coucou @${userstate.username} ! :)`);
+        }
+    },
+    {
+        name: "roll",
+        type: "function",
+        func: function (channel, userstate, message, client, sides = 20) {
+            let randNum = Math.floor(Math.random() * sides) + 1;
+            client.say(channel, `${userstate.username} rolled a ${randNum}!`);
+        }
+    },
+    // {
+    //     name: "social",
+    //     type: "message",
+    //     message: "Je ne suis nul part pour le moment."
+    // },
+    {
+        name: "tchip",
+        type: "message",
+        message: "/me tchipe tout le monde, sans exception."
+    }
+];
+module.exports = commands
